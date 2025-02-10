ca2e6d02dec84d4a3bdfff62665284a4428dc8d2
mabelKruger <mabelksouza@gmail.com>
2025-02-10 19:23:48 -0300

    first commit

---
+++ expenses/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+main() => runApp(const ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+  const ExpensesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: ExpensesPage(),
+    );
+  }
+}
+
+class ExpensesPage extends StatelessWidget {
+  const ExpensesPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Despesas pessoais'),
+      ),
+      body: const Center(
+        child: Text('Versão inicial'),
+      ),
+    );
+  }
+}
