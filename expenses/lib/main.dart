7d3e3c0cb5e563a17c03db15c8939e8ffa938a15
mabelKruger <mabelksouza@gmail.com>
2025-03-02 10:56:40 -0300

    styling text and container

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:expenses/models/transation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -19,12 +20,72 @@ class ExpensesPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final _transation = [
+      Transaction(
+        id: 't1',
+        title: 'title',
+        value: 1,
+        date: DateTime.now(),
+      ),
+      Transaction(
+        id: 't2',
+        title: 'title 2',
+        value: 2,
+        date: DateTime.now(),
+      ),
+    ];
     return Scaffold(
       appBar: AppBar(
         title: const Text('Despesas pessoais'),
       ),
-      body: const Center(
-        child: Text('Versão inicial'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          const Card(
+            child: Text('Gráfico'),
+          ),
+          Column(
+            children: _transation.map((tr) {
+              return Card(
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      margin: const EdgeInsets.symmetric(
+                        horizontal: 15,
+                        vertical: 10,
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.black,
+                          width: 1,
+                        ),
+                      ),
+                      padding: const EdgeInsets.all(10),
+                      child: Text(
+                        tr.value.toString(),
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Text(
+                          tr.title,
+                        ),
+                        Text(
+                          tr.date.toString(),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              );
+            }).toList(),
+          ),
+        ],
       ),
     );
   }
